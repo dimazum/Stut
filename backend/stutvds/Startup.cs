ae90eccb7ea7065ae7b3cc5c02bfc99f77184f90
Dzmitry Khileuski <dzmitry.khileuski@sphaera.ru>
2024-04-07 23:40:34 +0300

    update

--- backend/stutvds/Startup.cs
+++ backend/stutvds/Startup.cs
@@ -30,7 +30,7 @@ namespace stutvds
 			services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseSqlServer(
 					Configuration.GetConnectionString("Database")));
-			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
 				.AddEntityFrameworkStores<ApplicationDbContext>();
 			services.AddControllersWithViews();
 			services.AddRazorPages();
