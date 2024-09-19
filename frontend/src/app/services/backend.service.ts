fdf10a871712f33701e4ea61897d0cb9fdae8fef
Dzmitry Khileuski <dzmitry.khileuski@sphaera.ru>
2024-09-19 09:55:22 +0300

    upd

--- frontend/src/app/services/backend.service.ts
+++ frontend/src/app/services/backend.service.ts
@@ -8,7 +8,7 @@ import { ArticleData, Trigger, TriggerResult, TriggerTaskResult } from '../model
 })
 export class BackendService implements OnInit {
 
-  baseUrl = 'https://stut.mooo.com'
+  baseUrl = 'http://95.182.122.4:5000'
 
   constructor(private httpClient: HttpClient) { }
 
