21ffb42a3421846edf2a8b7a6f325d292536fd7c
Ivan Leushynski <dardillov999@gmail.com>
2025-11-23 15:40:14 +0300

    STU-15-Move backend project to SDK version 10

--- backend/stutvds.DAL/Data/ApplicationDbContext.cs
+++ backend/stutvds.DAL/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Internal;
 using StopStatAuth_6_0.Entities;
