aec249ed0cbb1ebbd148b2c1bb63a052232026e2
Ivan Leushynski <dardillov999@gmail.com>
2025-11-27 23:49:56 +0300

    add versioning

---
+++ backend/stutvds/Controllers/VersioningController.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using stutvds.Controllers.Base;
+
+namespace stutvds.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    
+    public class VersioningController : BaseController
+    {
+        [HttpGet]
+        [Route("current")]
+        public JsonResult GetCurrentVersion()
+        {
+            var entryAssembly = Assembly.GetEntryAssembly();
+
+            var version = entryAssembly?
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion;
+
+            return new JsonResult(version);
+        }
+    }
+}
