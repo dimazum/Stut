f96af9acd28b394042990fc609d64d312230095d
Dzmitry Khileuski <dzmitry.khileuski@modsen-software.com>
2025-11-28 19:30:08 +0300

    version test

--- backend/stutvds/Controllers/AuthController.cs
+++ backend/stutvds/Controllers/AuthController.cs
@@ -43,11 +43,7 @@ public class AuthController : ControllerBase
         var user = await _userManager.FindByNameAsync(model.Username);
         if (user == null || !await _userManager.CheckPasswordAsync(user, model.Password))
             return Unauthorized(new { message = "Invalid username or password" });
-
-        // Генерация JWT токена
-        
         var roles = await _userManager.GetRolesAsync(user);
-        
         var tokenHandler = new JwtSecurityTokenHandler();
         var key = Encoding.UTF8.GetBytes(_config.GetSection("Jwt:Key").Get<string>());
         
