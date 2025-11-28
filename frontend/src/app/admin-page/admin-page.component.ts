460f74a7cde08dafe9d7716494dcbd06bad486b4
Dzmitry Khileuski <dzmitry.khileuski@modsen-software.com>
2025-11-28 18:53:56 +0300

    add admin page

--- frontend/src/app/admin-page/admin-page.component.ts
+++ frontend/src/app/admin-page/admin-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../services/backend.service';
 
 @Component({
   selector: 'app-admin-page',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './admin-page.component.html',
   styleUrls: ['./admin-page.component.css']
 })
-export class AdminPageComponent {
+export class AdminPageComponent implements OnInit {
+
+  public version: string | undefined = undefined;
+
+  constructor(private backendService: BackendService) {
+  }
+
+  public ngOnInit(): void {
+    this.backendService.getCurrentVersion().subscribe(data => (this.version = data));
+  }
 
 }
