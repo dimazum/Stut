8b4bbe163233725f3164d57eab47b5a8283c5f3e
Dzmitry Khileuski <dzmitry.khileuski@modsen-software.com>
2025-12-27 22:45:36 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ echo "Commit hash: $COMMIT_HASH"
 export COMMIT_HASH=$COMMIT_HASH   # установить env
 
 echo "Cleaning up old Docker resources..."
-docker system prune -af --volumes   # -a для всех неиспользуемых образов, --volumes для томов, -f для подтверждения
+#docker image prune -af
 
 echo "Building Docker images..."
 docker-compose build              # теперь COMMIT_HASH доступен как build ARG
