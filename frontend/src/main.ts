21b5e9069adf9c9a58a816ab7e92bf2102db31b9
Dzmitry Khileuski <dzmitry.khileuski@modsen-software.com>
2025-12-31 16:32:49 +0300

    app-text

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,18 @@
-import 'zone.js';
+import 'zone.js';// for debug in razor
  
-
+import { Injector } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
 import { authInterceptor } from './app/auth.interceptor';
+import { AppComponent } from './app/app.component';
+import { createCustomElement } from '@angular/elements';
+import { RandomTextComponent } from './app/random-text/random-text.component';
+
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(withInterceptors([authInterceptor])), provideRouter(routes)],
-}).catch(err => console.error(err));
+  providers: [provideHttpClient(withInterceptors([authInterceptor]))],
+}).then(appRef => {
+  const injector = appRef.injector;
+  const el = createCustomElement(RandomTextComponent, { injector });
+  customElements.define('app-text', el);
+});
