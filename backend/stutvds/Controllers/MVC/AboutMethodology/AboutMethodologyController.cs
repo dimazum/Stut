80c1e7858ff2a2936796e80b464da8513721d53f
Dzmitry Khileuski <KhilevskiiDS@eurochem.ru>
2026-02-26 23:19:16 +0300

    upd

---
+++ backend/stutvds/Controllers/MVC/AboutMethodology/AboutMethodologyController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace stutvds.Controllers
+{
+	public class AboutMethodologyController : Controller
+	{
+
+		public AboutMethodologyController()
+		{
+		}
+
+		public IActionResult Index()
+		{
+			ViewData["Title"] = "Календарь прогресса речи — отслеживайте тренировки";
+			ViewData["Description"] = "Отслеживайте свой прогресс в упражнениях для дикции, дыхания и интонации с помощью календаря тренировок.";
+			ViewData["Keywords"] = "календарь, прогресс речи, упражнения, тренировка дикции, упражнения для голоса";
+
+			
+			return View();
+		}
+	}
+}
