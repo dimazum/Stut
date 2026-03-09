5bb1380e7e23144e50863c591eb4279d711ad092
Dzmitry Khileuski <KhilevskiiDS@eurochem.ru>
2026-03-09 09:57:25 +0300

    fix branch

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ GIT_USER="dimazum"
 GIT_TOKEN="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"
 
 echo "Pulling latest changes..."
-git pull https://${GIT_USER}:${GIT_TOKEN}@github.com/dimazum/Stut.git develop --tags
+git pull https://${GIT_USER}:${GIT_TOKEN}@github.com/dimazum/Stut.git main --tags
 
 COMMIT_HASH=$(git rev-parse --short HEAD)
 export COMMIT_HASH
