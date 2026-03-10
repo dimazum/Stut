7387189caeb945de977f5c84883d0f6cde33d718
Dzmitry Khileuski <KhilevskiiDS@eurochem.ru>
2026-03-10 14:45:51 +0300

    2 env

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Stut
 SpeechFlow
+
+Архитектура:
+
+SERVER
+│
+├── Traefik
+│
+├── PROD
+│   ├── backend
+│   ├── rabbitmq
+│   └── sqlserver (volume prod_sql_prod_data)
+│
+└── PREPROD
+    ├── backend
+    ├── rabbitmq
+    └── sqlserver (volume preprod_sql_preprod_data)
---
+++ deploy.preprod.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+echo "=============================="
+echo "Starting deployment preprod"
+echo "=============================="
+
+echo "Updating repository..."
+
+GIT_USER="dimazum"
+GIT_TOKEN="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"
+
+echo "Pulling latest changes..."
+git pull https://${GIT_USER}:${GIT_TOKEN}@github.com/dimazum/Stut.git develop --tags
+
+
+export COMMIT_HASH=$(git rev-parse --short HEAD)
+export APP_ENV=Production
+
+echo "Cleaning docker build cache..."
+
+docker image prune -f
+docker builder prune -f
+
+echo "Building Angular..."
+
+docker compose \
+-p preprod \
+-f docker-compose.yml \
+-f docker-compose.preprod.yml \
+build frontend-builder
+
+docker compose \
+-p preprod \
+-f docker-compose.yml \
+-f docker-compose.preprod.yml \
+run --rm frontend-builder
+
+echo "Starting containers..."
+
+docker compose \
+-p preprod \
+-f docker-compose.yml \
+-f docker-compose.preprod.yml \
+up -d --build
+
+echo "=============================="
+echo "✅ Deployment preprod finished successfully"
+echo "=============================="
